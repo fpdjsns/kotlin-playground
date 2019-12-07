aa20af7ed9c27dd4c3d73531ea349e394a53bafc
fpdjsns <withham1@naver.com>
2019-12-07 11:52:10 +0900

    add LocalDateTime TC truncate minutes unit

--- src/test/kotlin/kr/co/withhamit/LocalDateTimeTest.kt
+++ src/test/kotlin/kr/co/withhamit/LocalDateTimeTest.kt
@@ -3,6 +3,8 @@ package kr.co.withhamit
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
 import java.time.LocalDateTime
+import java.time.temporal.ChronoUnit
+import kotlin.test.assertNotEquals
 
 /**
  * @author Hyunsu Bae
@@ -14,9 +16,14 @@ class LocalDateTimeTest {
 
     // expect
     assertEquals(LocalDateTime.of(2019, 12, 4, 12, 3, 12), "2019-12-04 12:03:12".toLocalDateTime())
-    assertEquals(LocalDateTime.of(2019, 12, 4, 12, 3), "2019-12-04 12:03:12".toLocalDateTime()?.withSecond(0))
     assertEquals(LocalDateTime.of(2019, 12, 4, 12, 3), "2019-12-04 12:03".toLocalDateTime())
-    assertEquals(LocalDateTime.of(2019, 12, 4, 12, 3,12,100000000), "2019-12-04 12:03:12.1".toLocalDateTime())
+    assertEquals(LocalDateTime.of(2019, 12, 4, 12, 3, 12, 100000000), "2019-12-04 12:03:12.1".toLocalDateTime())
 
+    assertEquals(LocalDateTime.of(2019, 12, 4, 12, 3),
+        "2019-12-04 12:03:12".toLocalDateTime()?.withSecond(0))
+    assertNotEquals(LocalDateTime.of(2019, 12, 4, 12, 3),
+        "2019-12-04 12:03:12.1".toLocalDateTime()?.withSecond(0))
+    assertEquals(LocalDateTime.of(2019, 12, 4, 12, 3),
+        "2019-12-04 12:03:12.1".toLocalDateTime()?.truncatedTo(ChronoUnit.MINUTES))
   }
 }
